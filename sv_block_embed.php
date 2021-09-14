55af8d7c0e6a67142a719a3abf1a4ce1db359b8f
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-09-14 18:09:01 +0200

    init

---
+++ sv_block_embed.php
@@ -0,0 +1,56 @@
+<?php
+	namespace sv100;
+
+	class sv_block_embed extends init {
+		public function init() {
+			$this->set_module_title( __( 'Block: Embed', 'sv100' ) )
+				->set_module_desc( __( 'Settings for Gutenberg Block', 'sv100' ) )
+				->set_css_cache_active()
+				->set_section_title( $this->get_module_title() )
+				->set_section_desc( $this->get_module_desc() )
+				->set_section_template_path()
+				->set_section_order(5000)
+				->set_section_icon('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/></svg>')
+				->get_root()
+				->add_section( $this );
+		}
+
+		protected function load_settings(): sv_block_embed {
+			$this->get_setting( 'margin' )
+				->set_title( __( 'Margin', 'sv100' ) )
+				->set_is_responsive(true)
+				->set_default_value(array(
+					'top'		=> '0',
+					'right'		=> 'auto',
+					'bottom'	=> '0',
+					'left'		=> 'auto'
+				))
+				->load_type( 'margin' );
+
+			$this->get_setting( 'padding' )
+				->set_title( __( 'Padding', 'sv100' ) )
+				->set_is_responsive(true)
+				->load_type( 'margin' );
+
+			$this->get_setting( 'border' )
+				->set_title( __( 'Border', 'sv100' ) )
+				->set_is_responsive(true)
+				->load_type( 'border' );
+
+			return $this;
+		}
+		public function enqueue_scripts(): sv_block_embed {
+			if(!$this->has_block_frontend('embed')){
+				return $this;
+			}
+
+			if(!is_admin()){
+				$this->load_settings()->register_scripts();
+			}
+
+			$this->get_script( 'common' )->set_is_enqueued();
+			$this->get_script( 'config' )->set_is_enqueued();
+
+			return $this;
+		}
+	}
