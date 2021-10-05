2ed4ca9f4abec0929dda96029ecf9b2c6daf61e2
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-10-05 20:09:01 +0200

    ADD GUTENBERG DEFAULT STYLES

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-embed' : '.sv100_sv_content_wrapper article .wp-block-embed',
+		is_admin() ? '.editor-styles-wrapper .wp-block-embed' : '.wp-block-embed',
 		array_merge(
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('margin')->get_css_data(),
